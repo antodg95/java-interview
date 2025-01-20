0044d0d8f7b7b6a1bcdee471943420d2577fdc66
Antonio Di Giulio <antodg95@gmail.com>
2025-01-20 16:40:39 +0000

    first try

---
+++ java-interview/src/main/java/com/example/java_interview/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.example.java_interview.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class MainController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "Hello world!";
+    }
+
+}
